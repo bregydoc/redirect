5fe57ca8593e2268664d69545191e3b604204dc5
Bregy Malpartida <bregy.malpartida@utec.edu.pe>
2020-02-05 15:03:21 -0500

    added dynamic route to env config

--- main.py
+++ main.py
@@ -4,11 +4,12 @@ from flask import Flask, redirect
 focus_url = os.environ.get('REDIRECT_URL', "http://www.example.com")
 code_redirect = os.environ.get('REDIRECT_CODE', 302)
 port = os.environ.get('PORT', 8080)
+route = os.environ.get('ROUTE', '/')
 
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route(route)
 def hello():
     return redirect(focus_url, code=code_redirect)
 
